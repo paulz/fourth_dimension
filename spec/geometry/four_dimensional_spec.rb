e0a84a1352b8be86fa7767d4486cd07b1d524a47
unknown <Pasha@.(none)>
2009-03-04 02:41:38 -0800

    refactor test

--- spec/geometry/four_dimensional_spec.rb
+++ spec/geometry/four_dimensional_spec.rb
@@ -8,29 +8,48 @@ describe Geometry::Space do
       @pentachoron = @four_dimensions.simplex(1)
     end
 
-    it "#volume" do
-      @pentachoron.volume.should be_close(0.02329237, 0.00001)
-      @pentachoron.vertices.should == 5
-      # See formular at http://en.wikipedia.org/wiki/Simplex
-      # http://upload.wikimedia.org/math/1/0/4/10457cdca57c10861902004469b9cf02.png
-      @pentachoron.volume.should be_close(@pentachoron.calc_simplex_formula, ::Float::EPSILON)
-      @four_dimensions.simplex(2).calc_simplex_formula.should == 16 * @pentachoron.calc_simplex_formula
-      @four_dimensions.simplex(2).hight_from_simple_formula.should be_close(2 * @pentachoron.hight, 2 * ::Float::EPSILON)
+    context "#simplex" do
+      it "#vertices" do
+        @pentachoron.vertices.should == 5
+      end
 
-      @pentachoron.calc_volume(1000).should be_close(0.02329237, 0.001)
+      it "#volume" do
+        @four_dimensions.simplex(2).volume.should == 16 * @pentachoron.volume
 
-      @four_dimensions.cube(1).volume.should == 1
-      @four_dimensions.cube(2).volume.should == 16 * @four_dimensions.cube(1).volume
-      @four_dimensions.simplex(2).volume.should == 16 * @pentachoron.volume
-      @four_dimensions.simplex(1).hight_from_simple_formula.should be_close(@four_dimensions.simplex(1).hight, ::Float::EPSILON)
+        @pentachoron.volume.should be_close(0.0232923747656228, ::Float::EPSILON)
+      end
+
+      it "#calc_simplex_formula" do
+        # See formular at http://en.wikipedia.org/wiki/Simplex
+        # http://upload.wikimedia.org/math/1/0/4/10457cdca57c10861902004469b9cf02.png
+        @pentachoron.calc_simplex_formula.should be_close(@pentachoron.volume, ::Float::EPSILON)
+      end
+
+      it "#calc_volume" do
+        @pentachoron.calc_volume(1000).should be_close(@pentachoron.volume, 0.001)
+      end
+
+      it "#height" do
+        hight = @pentachoron.hight
+        hight.should be_close(0.790569415042095, ::Float::EPSILON)
+        @pentachoron.calc_hight(10000).should be_close(hight, 0.0001)
+      end
+
+      it "#hight_from_simple_formula" do
+        @pentachoron.hight_from_simple_formula.should be_close(@pentachoron.hight, ::Float::EPSILON)
+        @four_dimensions.simplex(2).hight_from_simple_formula.should be_close(2 * @pentachoron.hight, 2 * ::Float::EPSILON)
+      end
     end
 
-    it "#height" do
-      hight = @pentachoron.hight
-      hight.should be_close(0.790569415042095,
-              0.0000000000000003)
+    context "#cube" do
+      it "#vertices" do
+        @four_dimensions.cube(7).vertices.should == 16
+      end
 
-      @pentachoron.calc_hight(10000).should be_close(hight, 0.0001)
+      it "#volume" do
+        @four_dimensions.cube(1).volume.should == 1
+        @four_dimensions.cube(10).volume.should == 16 * @four_dimensions.cube(5).volume
+      end
     end
   end
 
