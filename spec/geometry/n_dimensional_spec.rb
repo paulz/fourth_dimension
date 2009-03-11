af75b6013aa7a7f57145e57134787185892935bf
unknown <Pasha@.(none)>
2009-03-10 23:07:09 -0700

    refactor shared spec

--- spec/geometry/n_dimensional_spec.rb
+++ spec/geometry/n_dimensional_spec.rb
@@ -14,12 +14,12 @@ require File.dirname(__FILE__)+ '/../spec_helper.rb'
       end
     end
 
-    share_examples_for "any two similar figures in #{n} dimentions" do
+    share_examples_for "similar #{n} dimensional figures" do
       it "volume should be proportional to it's side in power of #{n}" do
         (@figure1.volume / @figure2.volume).should be_close( (@figure1.side / @figure2.side) ** n, ::Float::EPSILON)
       end
 
-      it "number of vertices is independent of a side" do
+      it "should have same number of vertices" do
         @figure1.vertices.should == @figure2.vertices
       end
     end
@@ -37,7 +37,7 @@ require File.dirname(__FILE__)+ '/../spec_helper.rb'
           @figure2 = @space.cube(7)
         end
 
-        it_should_behave_like "any two similar figures in #{n} dimentions"
+        it_should_behave_like "similar #{n} dimensional figures"
       end
     end
 
@@ -55,16 +55,16 @@ require File.dirname(__FILE__)+ '/../spec_helper.rb'
 
       describe "#hight" do
         it "should be smaller then a side" do
-          @space.simplex(1).hight.should < @space.simplex(1).side 
+          @space.simplex(1).height.should < @space.simplex(1).side
         end
 
         it "should be proportional to the side" do
-          (@figure1.hight/@figure2.hight).should be_close(@figure1.side/@figure2.side, ::Float::EPSILON) 
+          (@figure1.height/@figure2.height).should be_close(@figure1.side/@figure2.side, ::Float::EPSILON) 
         end
       end
 
       describe "#volume" do
-        it_should_behave_like "any two similar figures in #{n} dimentions"
+        it_should_behave_like "similar #{n} dimensional figures"
       end
     end
   end
